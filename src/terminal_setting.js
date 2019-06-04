e44d12f4b022ecea2cc55d5c2a05872d212f937a
Tatsuemon <110manns@gmail.com>
2019-06-04 23:29:32 +0900

    try

--- src/terminal_setting.js
+++ src/terminal_setting.js
@@ -1,18 +1,38 @@
-Terminal.applyAddon(attach);
-Terminal.applyAddon(fit);
-Terminal.applyAddon(fullscreen);
-
-const term = new Terminal({fontFamily: "Menlo for Powerline"});
-const container = document.getElementById('terminal');
-term.open(container);
-term.fit();
-term.toggleFullScreen(true);
-
-
 // httpsだったらwss://, それ以外はws://
-const protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
+const protocol = location.protocol
 const port = location.port ? `:${location.port}` : '';
-const socketUrl = `${protocol}${location.hostname}${port}/shell`;
-const socket = new WebSocket(socketUrl);
+const socketUrl = `${protocol}${location.hostname}${port}`;
+
+let socket = io.connect(socketUrl);
+socket.on("connect", function(){
+    console.log(socket);
+    Terminal.applyAddon(attach);
+    Terminal.applyAddon(fit);
+    Terminal.applyAddon(fullscreen);
+
+    const term = new Terminal({fontFamily: "Menlo for Powerline"});
+    term.on("data", function(data){
+        socket.emit("message", data);
+    })
+
+    socket.on("data", function(data){
+        term.write(data);
+    })
+    const container = document.getElementById('terminal');
+    term.open(container);
+    term.fit();
+    term.toggleFullScreen(true);
+})
+
+// // httpsだったらwss://, それ以外はws://
+// const protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
+// const port = location.port ? `:${location.port}` : '';
+// const socketUrl = `${protocol}${location.hostname}${port}/shell`;
+// const socket = new WebSocket(socketUrl);
+// console.log(socket)
+// var sscket = io.connect()
 // これでWebSocketStreamとTerminalを繋げるかつopen
-socket.onopen = (ev) => { term.attach(socket) };
+// socket.onopen = () => { 
+//     console.log(socket)
+//     term.attach(socket)
+// };
